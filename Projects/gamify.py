f74c878943a07e0fc49aa54d389634e24baca18e
Ted <p_t@fastmail.net>
2020-09-28 23:57:00 -0400

    Update gamify.py

--- Projects/gamify.py
+++ Projects/gamify.py
@@ -4,6 +4,25 @@ activities = {"running": {"health": ((3, 180), (1,)), "hedons": ((2, 10), (-2,))
               }
 
 
+def initialize():
+    """Initialize all global variables"""
+    global hedons
+    global health
+    global time_since_tiring
+    global star
+    global last_two_stars
+    global bored
+    global previous_activity
+
+    hedons = 0
+    health = 0
+    time_since_tiring = 120
+    star = None
+    last_two_stars = [120, 120]
+    bored = False
+    previous_activity = [None, 0]
+
+
 def is_timed(item):
     """Check if a stat item has a timed increment. item = (x,) or (x, y)"""
     try:
@@ -98,44 +117,3 @@ def star_can_be_taken(activity):
 def most_fun_activity_minute():
     """Return the activity which would give the most hedones if preformed for one minute"""
     return sorted(activities.keys(), key=lambda activity: (activities[activity]["hedons"][0][0] if time_since_tiring >= 120 or not activities[activity]["is_tiring"] else -2)+3*(star == activity), reverse=True)[0]
-
-
-def initialize():
-    """Initialize all global variables"""
-    global hedons
-    global health
-    global time_since_tiring
-    global star
-    global last_two_stars
-    global bored
-    global previous_activity
-
-    hedons = 0
-    health = 0
-    time_since_tiring = 120
-    star = None
-    last_two_stars = [120, 120]
-    bored = False
-    previous_activity = [None, 0]
-
-
-
-if __name__ == '__main__':
-    initialize()
-    perform_activity("running", 30)
-    print(get_cur_hedons())            # -20 = 10 * 2 + 20 * (-2)
-    print(get_cur_health())            # 90 = 30 * 3
-    print(most_fun_activity_minute())  #resting
-    perform_activity("resting", 30)
-    offer_star("running")
-    print(most_fun_activity_minute())  # running
-    perform_activity("textbooks", 30)
-    print(get_cur_health())            # 150 = 90 + 30*2
-    print(get_cur_hedons())            # -80 = -20 + 30 * (-2)
-    offer_star("running")
-    perform_activity("running", 20)
-    print(get_cur_health())            # 210 = 150 + 20 * 3
-    print(get_cur_hedons())            # -90 = -80 + 10 * (3-2) + 10 * (-2)
-    perform_activity("running", 170)
-    print(get_cur_health())            # 700 = 210 + 160 * 3 + 10 * 1
-    print(get_cur_hedons())            # -430 = -90 + 170 * (-2)
