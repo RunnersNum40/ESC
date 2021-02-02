1d6ececf528845421717d191dc90076ff3dd0c7c
Ted Pinkerton <71232626+RunnersNum40@users.noreply.github.com>
2021-02-01 22:18:36 -0500

    Delete Labs directory

--- Labs/lab4-golf.py
+++
@@ -1,5 +0,0 @@
-e=lambda L:[n%2 for n in L].count(0)
-l=lambda x:"[%s]"%(",".join(map(str,x)))
-s=lambda o,t:len(o)==len(t)and all(x==y for x,y in zip(o, t))
-
-print(repr(l([1, 2, 3, 4])))
--- Labs/test.py
+++
@@ -1,5 +0,0 @@
-def mul(A, B): return [[sum([n*i for n, i in zip(A[x], [row[y] for row in B])]) for y in range(len(A))] for x in range(len(A))]
-
-A = [[1, 2, 3], [4, 5, 6]]
-B = [[7, 8], [9, 10], [11, 12]]
-print(mul(A, B))
