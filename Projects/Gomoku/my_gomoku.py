66c9fb95758d7b4c933ac99cfdadbfd875bf0032
Ted Pinkerton <71232626+RunnersNum40@users.noreply.github.com>
2021-02-01 22:18:59 -0500

    Delete Projects directory

--- Projects/Gomoku/my_gomoku.py
+++
@@ -1,7 +0,0 @@
-board = [[" " for x in range(8)] for y in range(8)]
-
-
-
-def is_bounded(board, y_end, x_end, length, d_y, d_x):
-	board = [row+["b"] for row in board] + [["b"]*9]
-	return ["CLOSED", "SEMIOPEN", "OPEN"][(board[y_end+d_y][x_end+d_x] == " ")+(board[y_end-length*d_y-d_y][x_end-length*d_x-d_x] == " ")]
